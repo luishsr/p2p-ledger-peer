bb0bf7ccd89829568b3cfa6ea6fa51397bf22d25
Luis Soares <80909424+luishsr@users.noreply.github.com>
2023-09-02 17:11:57 -0300

    Update main.rs

--- main.rs
+++ main.rs
@@ -3,8 +3,14 @@ use p2pledger::{Peer};
 
 fn main() {
     // Define own peer address and create a new Peer instance
-    let peer_address = String::from("127.0.0.1:8085");
-    let peer = Peer::new(peer_address.clone(), blockchain);
-    //sds
+    let peer_address = String::from("127.0.0.1:8083");
+    let peer = Peer::new(peer_address.clone());
+    
+    // Start listening for incoming connections
+    tokio::spawn(async move {
+        peer.listen().await;
+    });
+
+    //peer.connect(PEER_ADDRESS).await;
 
 }
